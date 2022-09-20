35dc5e9fb3e8bce00a445978f0c07c4fcfab4119
mig <michaelleojacob@gmail.com>
2022-09-19 22:17:00 -0700

    added errors.
    errors initially throwing x2 errors per invalid input validation. Fixed by putting show only first error as true

--- routes/form.js
+++ routes/form.js
@@ -1,15 +1,42 @@
 const express = require('express');
 const router = express();
 const sendMail = require('../utils/sendmail.js');
+const { body, validationResult } = require('express-validator');
 
 router.get('/', (req, res, next) => {
   res.render('form');
 });
 
-router.post('/', (req, res, next) => {
-  const { first, last, phone, email } = req.body;
-  sendMail(first, last, email, phone);
-  res.redirect('/');
-});
+router.post('/', [
+  body('first', 'first name is required').trim().isLength({ min: 1 }).escape(),
+  body('last', 'last name is required').trim().isLength({ min: 1 }).escape(),
+  body('phone', 'phone number is invalid')
+    .trim()
+    .isLength({ min: 9 })
+    .isMobilePhone()
+    .escape(),
+  body('email', 'email is invalid')
+    .trim()
+    .isLength({ min: 3 })
+    .isEmail()
+    .escape(),
+  (req, res, next) => {
+    const errors = validationResult(req);
+    const { first, last, phone, email } = req.body;
+    // there are errors
+    if (!errors.isEmpty()) {
+      res.render('form', {
+        first,
+        last,
+        phone,
+        email,
+        errors: errors.array({ onlyFirstError: true }),
+      });
+      return;
+    }
+    // sendMail(first, last, email, phone);
+    res.redirect('/');
+  },
+]);
 
 module.exports = router;
